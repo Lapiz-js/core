4a3a37fd5837fa3e354c97c7cc46e674722fe9bd
Adam Colton <adamcolton@gmail.com>
2016-05-16 16:48:28 -0400

    improving documentation

--- collectionsHelper.js
+++ collectionsHelper.js
@@ -1,15 +1,40 @@
 Lapiz.Module("Collections", function($L){
+  // > Lapiz.Map()
+  // Returns a key value store that inherits no properties or methods. Useful to
+  // bypass calling "hasOwnProperty". This is just a wrapper around
+  // Object.create(null);
+  //
+  // Lapiz.Map also serves as a namespace for the following helper methods.
+  // They can be called on any object. They all use Object.defineProperty to
+  // create a proptery that cannot be overridden.
   function Map(){
     return Object.create(null);
   };
   $L.set($L, "Map", Map);
 
+  // > Lapiz.Map.method(obj, namedFunc)
+  // Attaches a method to an object. The method must be a named function.
+  /* >
+  var x = Lapiz.Map();
+  Lapiz.Map.method(x, function foo(){...});
+  x.foo(); //calls foo
+  */
   $L.set(Map, "method", function(obj, fn){
     $L.typeCheck.function(fn, "Expected function");
     $L.assert(fn.name !== "", "Require named function for method");
     $L.set(obj, fn.name, fn);
   });
 
+  // > Lapiz.Map.setterMethod(obj, namedSetterFunc)
+  // Attaches a setter method to an object. The method must be a named function.
+  /* >
+  var x = Lapiz.Map();
+  Lapiz.Map.method(x, function foo(val){...});
+
+  //these two calls are equivalent
+  x.foo("bar");
+  x.foo = "bar";
+  */
   Map.method(Map, function setterMethod(obj, fn){
     $L.typeCheck.function(fn, "Expected function for setterMethod");
     $L.assert(fn.name !== "", "Require named function for setterMethod");
@@ -19,16 +44,61 @@ Lapiz.Module("Collections", function($L){
     });
   });
 
+  // > Lapiz.Map.prop(obj, name, desc)
+  // Just a wrapper around Object.defineProperty
   Map.method(Map, function prop(obj, name, desc){
     Object.defineProperty(obj, name, desc);
   });
 
+  // > Lapiz.Map.getter(object, namedGetterFunc)
+  // Attaches a getter method to an object. The method must be a named function.
+  /* >
+  var x = Lapiz.Map();
+  var ctr = 0;
+  Lapiz.Map.getter(x, function foo(){
+    var c = ctr;
+    ctr +=1;
+    return c;
+  });
+  console.log(x.foo); //0
+  console.log(x.foo); //1
+  */
   Map.method(Map, function getter(obj, fn){
     $L.typeCheck.function(fn, "Expected function for getter");
     $L.assert(fn.name !== "", "Require named function for getter");
     Object.defineProperty(obj, fn.name, {"get": fn,} );
   });
 
+  // > Lapiz.Map.setterGetter(obj, name, setterFunc, getterFunc)
+  // > Lapiz.Map.setterGetter(obj, name, setterFunc)
+  // Creates a setter/getter property via a closure. A setter function is
+  // required, if no getter is provided, the value will be returned. This is the
+  // reason the method is named setterGetter rather than the more traditional
+  // arrangement of "getterSetter" because the arguments are arranged so that
+  // the first 3 are required and the last is optional.
+  /* >
+  var x = Lapiz.Map();
+  Lapiz.Map.setterGetter(x, "foo", function(i){return parseInt(i);});
+
+  x.foo = "12";
+  console.log(x.foo); // will log 12 as an int
+  */
+  // The value 'this' is always set to a special setterInterface for the setter
+  // method. This can be used to cancel the set operation;
+  /* >
+  var x = Lapiz.Map();
+  Lapiz.Map.setterGetter(x, "foo", function(i){
+    i = parseInt(i);
+    this.set = !isNaN(i);
+    return i;
+  });
+
+  x.foo = "12";
+  console.log(x.foo); // will log 12 as an int
+  x.foo = "hello";
+  console.log(x.foo); // value will still be 12
+
+  */
   Map.method(Map, function setterGetter(obj, name, setter, getter){
     $L.typeCheck.function(setter, "Expected function for setterGetter");
     var val;
@@ -55,6 +125,20 @@ Lapiz.Module("Collections", function($L){
     Object.defineProperty(obj, name, desc);
   });
 
+  // > Lapiz.Map.copyProps(copyTo, copyFrom, props...)
+  // Copies the properties from the copyFrom object to the copyTo obj. The
+  // properties should be strings. By default, the property will be copied with
+  // basic assignment. If the property is preceeded by &, it will be copied by
+  // reference.
+  /* >
+  var A = {"x": 12, "y": "foo", z:[]};
+  var B = {};
+  Lapiz.Map.copyProps(B, A, "x", "&y");
+  A.x = 314;
+  console.log(B.x); // 12
+  B.y = "Test";
+  console.log(A.y); // Test
+  */
   Map.method(Map, function copyProps(copyTo, copyFrom){
     //todo: write tests for this
     var i = 2;
@@ -78,6 +162,43 @@ Lapiz.Module("Collections", function($L){
     }
   });
 
+  // > Lapiz.Namespace()
+  // > Lapiz.Namespace(constructor)
+  // Namespace is a closure around all of the Map methods (plus Lapiz.set). It
+  // provides syntactic sugar so that the obj argument doesn't need to be
+  // supplied each time.
+  //
+  // The constructor is optional. If not given the outer layer of the namespace
+  // is returned.
+  /* >
+  var x = Lapiz.Namespace();
+  x.set("foo", "bar");
+  x.method(function sayHello(name){
+    console.log("Hello, "+name);
+  });
+  console.log(x.namespace.foo); // bar
+  x.namespace.sayHello("World"); // Hello, World
+  */
+  // If a constructor is provided, it will be invoked with "this" as the outer
+  // layer of the namespace and will return in the inner namespace.
+  /* >
+  var x = Lapiz.Namespace(function(){
+    this.set("foo", "bar");
+    this.method(function sayHello(name){
+      console.log("Hello, "+name);
+    });
+  });
+
+  console.log(x.foo); // bar
+  x.sayHello("World"); // Hello, World
+  */
+  // * namespace.set(name, value)
+  // * namespace.prop(name, desc)
+  // * namespace.method(namedFunc)
+  // * namespace.setterMethod(namedSetterFunc)
+  // * namespace.getter(namedGetterFunc)
+  // * namespace.setterGetter(name, setter, getter)
+  // * namespace.setterGetter(name, setter)
   Map.method($L, function Namespace(fn){
     var self = $L.Map();
     self.namespace = $L.Map();
@@ -96,11 +217,42 @@ Lapiz.Module("Collections", function($L){
     return self;
   });
 
+  // > Lapiz.remove(arr, el, start)
+  // > Lapiz.remove(arr, el)
+  // Removes the one instance of the given element from the array. If start is
+  // not specified, it will be the first instance, otherwise it will be the
+  // first instance at or after start.
+  /* >
+  var arr = [3,1,4,1,5,9];
+  Lapiz.remove(arr,1);
+  console.log(arr); //[3,4,1,5,9]
+  */
   Map.method($L, function remove(arr, el, start){
     var i = arr.indexOf(el, start);
     if (i > -1) { arr.splice(i, 1); }
   });
 
+  // > Lapiz.each(collection, fn(key, val))
+  // Iterates over the collection, calling func(key, val) for each item in the
+  // collection. If the collection is an array, key will be the index. If func
+  // returns true (or an equivalent value) the Lapiz.each will return the
+  // current key allowing each to act as a search.
+  /* >
+  var arr = [3,1,4,1,5,9];
+  Lapiz.each(arr, function(key,val){
+    console.log(key, val);
+  });
+  var gt4 = Lapiz.each(arr, function(key,val){return val > 4;});
+
+  var kv = {
+    "A":"apple",
+    "B":"banana",
+    "C":"cantaloupe"
+  };
+  Lapiz.each(kv, function(key,val){
+    console.log(key, val);
+  });
+  */
   Map.method($L, function each(obj, fn){
     var i;
     if (obj instanceof Array){
@@ -117,6 +269,12 @@ Lapiz.Module("Collections", function($L){
     }
   });
 
+  // > Lapiz.ArrayConverter(accessor)
+  // Takes an accessor and provides an array. The events on the accessor will be
+  // used to keep the array up to date. However, if the array is modified, the
+  // results can be unpredictable. This primarily provided as a tool for
+  // interfacing with other libraries and frameworks. Use the accessor interface
+  // whenever possible.
   Map.method($L, function ArrayConverter(accessor){
     var arr = [];
     var index = [];
--- init.js
+++ init.js
@@ -1,16 +1,17 @@
-/**
- * @namespace Lapiz
- */
 var Lapiz = (function($L) {
   return $L || Object.create(null);
 }(Lapiz));
 
-/**
- * @namespace ModuleLoaderModule
- * @memberof Lapiz
- *
- * Loads modules into Lapiz, allowing them to declare dependencies
- */
+// > Lapiz.Module(moduleName, moduleFunction(Lapiz))
+// > Lapiz.Module(moduleName, [dependencies...], moduleFunction(Lapiz))
+// The module loader is useful when building Lapiz modules. It will invoke the
+// moduleFunction and pass in Lapiz when all dependencies have been loaded. The
+// moduleName is only used for dependency management.
+/* >
+Lapiz.Module("Foo", ["Events"], function($L){
+  $L.set($L, "foo", "bar");
+});
+*/
 var Lapiz = (function ModuleLoaderModule($L){
   var _loaded = Object.create(null);
   var _pending = [];
@@ -26,6 +27,19 @@ var Lapiz = (function ModuleLoaderModule($L){
     return notLoaded;
   };
 
+  // > Lapiz.set(obj, name, value)
+  // Defines a fixed propery on an object. Properties defined this way cannot be
+  // overridden.
+  /* >
+  var x = {};
+  x.foo = function(){...};
+  //somewhere else
+  x.foo = 12; //the method is now gone
+
+  var y = {};
+  Lapiz.set(y, "foo", function{...});
+  y.foo = 12; // this will not override the method
+  */
   function set(obj, name, value){
     Object.defineProperty(obj, name, { value: value });
   }
@@ -76,11 +90,25 @@ var Lapiz = (function ModuleLoaderModule($L){
     }
   });
 
+  // > Lapiz.Module.Loaded()
+  // Returns all the modules that have been loaded
   $L.Module.Loaded = function(){
     return Object.keys(_loaded);
   };
   Object.freeze(self.Module);
 
+  // > Lapiz.typeCheck(obj, type)
+  // > Lapiz.typeCheck(obj, type, err)
+  // Checks if the type of obj matches type. If type is a string, typeof will be
+  // used, if type is a class, instanceof will be used. To throw an error when
+  // the types do not match, specify err as a string. Other wise, typeCheck will
+  // return a boolean indicating if the types matched.
+  /* >
+  Lapiz.typeCheck([], Array); // true
+  Lapiz.typeCheck("test", "string"); // true
+  Lapiz.typeCheck("test", Array); // false
+  Lapiz.typeCheck([], "string", "Expected string"); // throws an error
+  */
   $L.set($L, "typeCheck", function(obj, type, err){
     var typeCheck = (typeof type === "string") ? (typeof obj === type) : (obj instanceof type);
     if (err !== undefined && !typeCheck){
@@ -88,11 +116,33 @@ var Lapiz = (function ModuleLoaderModule($L){
     }
     return typeCheck;
   });
+
+  // > Lapiz.typeCheck.function(obj)
+  // > Lapiz.typeCheck.function(obj, err)
+  // Checks if the object is a function. If a string is supplied for err, it
+  // will throw err if obj is not a function.
   $L.set($L.typeCheck, "function", function(obj, err){return $L.typeCheck(obj, Function, err)});
+
+  // > Lapiz.typeCheck.array(obj)
+  // > Lapiz.typeCheck.array(obj, err)
+  // Checks if the object is a array. If a string is supplied for err, it
+  // will throw err if obj is not an array.
   $L.set($L.typeCheck, "array", function(obj, err){return $L.typeCheck(obj, Array, err)});
+
+  // > Lapiz.typeCheck.string(obj)
+  // > Lapiz.typeCheck.string(obj, err)
+  // Checks if the object is a string. If a string is supplied for err, it
+  // will throw err if obj is not an string.
   $L.set($L.typeCheck, "string", function(obj, err){return $L.typeCheck(obj, "string", err)});
+
+  // > Lapiz.typeCheck.number(obj)
+  // > Lapiz.typeCheck.number(obj, err)
+  // Checks if the object is a number. If a string is supplied for err, it
+  // will throw err if obj is not an number.
   $L.set($L.typeCheck, "number", function(obj, err){return $L.typeCheck(obj, "number", err)});
 
+  // > Lapiz.assert(bool, err)
+  // If bool evaluates to false, an error is thrown with err.
   $L.set($L, "assert", function(bool, err){
     if (!bool){
       throw new Error(err);
