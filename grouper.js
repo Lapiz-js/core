e3354f7636ba9b984fd8653b6d7582ed9d0c8f6c
Adam Colton <adamcolton@gmail.com>
2018-05-05 16:46:15 -0400

    Added GroupBy collection

--- grouper.js
+++ grouper.js
@@ -1,9 +1,13 @@
 // > .ModuleName "Group"
 Lapiz.Module("Group", function($L){
 
-  // > Lapiz.Group(accessor)
-  // Creates a sub-group of values from an accessor.
-  $L.set($L, "Group", function(accessor){
+  // > settings
+  // Settings to change the behavior of the group
+
+  // > Lapiz.Group(accessor, settings)
+  // Creates a sub-group of values from an accessor. This is essentially an
+  // actively managed filter.
+  $L.set($L, "Group", function(accessor, settings){
     var _index = [];
 
     // > group(key)
@@ -15,10 +19,6 @@ Lapiz.Module("Group", function($L){
     // > group._cls
     // Return Lapiz.Group
     $L.set(self, "_cls", $L.Group);
-
-    // > group.Accessor
-    // Returns a reference to self
-    $L.set(self, "Accessor", self);
 
     // > group.Sort(sorterFunction)
     // > group.Sort(fieldName)
@@ -96,40 +96,255 @@ Lapiz.Module("Group", function($L){
       }
     }
 
-    accessor.on.remove(remFn);
-    accessor.on.change(changeFn);
+    // > settings.DoNotListen
+    if (settings === undefined || settings.DoNotListen !== true ){
+      accessor.on.remove(remFn);
+      accessor.on.change(changeFn);
 
-    // > group.kill()
-    // After calling kill, a Filter is no longer live. It will not receive
-    // updates and can more easily be garbage collected (because it's
-    // parent accessor no longer has any references to it).
-    $L.set.meth(self, function kill(){
-      accessor.on.remove.deregister(remFn);
-      accessor.on.change.deregister(changeFn);
-    });
+      // > group.kill()
+      // After calling kill, a Filter is no longer live. It will not receive
+      // updates and can more easily be garbage collected (because it's
+      // parent accessor no longer has any references to it).
+      $L.set.meth(self, function kill(){
+        accessor.on.remove.deregister(remFn);
+        accessor.on.change.deregister(changeFn);
+      });
+    }
 
 
     // > group.Add(key)
     // If the key exists in the accessor, it is added to the group.
     $L.set.meth(self, function Add(key){
       key = key.toString();
-      if (accessor.has(key)){
+      if (accessor.has(key) && _index.indexOf(key) === -1){
         _index.push(key);
         _insertEvent.fire(key, self);
       }
     });
 
     // > group.Remove(key)
-    $L.set.meth(self, function Remove(key){
+    // > group.Remove(key, oldVal)
+    // Removes the key. The oldVal parameter is only needed if the Group is not
+    // listening on it's underlying Accessor and the value has been removed.
+    $L.set.meth(self, function Remove(key, oldVal){
       key = key.toString();
       var i = _index.indexOf(key);
+      if (oldVal === undefined){
+        oldVal = accessor(key);
+      }
       if (i > -1){
         _index.splice(i, 1);
-        _removeEvent.fire(key, self, accessor(key));
+        _removeEvent.fire(key, self, oldVal);
       }
     });
 
+    // > group.Change(key, oldVal)
+    // Fires the change event.
+    $L.set.meth(self, function Change(key, oldVal){
+      changeFn(key, accessor, oldVal)
+    })
+
+    // > group.Accessor
+    // > group.Accessor(key)
+    // The accessor is a read-only iterface to the group
+    //
+    // * accessor.length
+    // * accessor.keys
+    // * accessor.has(key)
+    // * accessor.each(fn(val, key))
+    // * accessor.on.insert
+    // * accessor.on.change
+    // * accessor.on.remove
+    // * accessor.Sort
+    // * accessor.Filter
+    $L.set(self, function Accessor(){
+      return self(key);
+    });
+    $L.set.copyProps(self.Accessor, self, "Accessor", "&length", "has", "each", "on", "Sort", "Filter", "&keys");
+    self.Accessor._cls = $L.Accessor;
+
     Object.freeze(self);
+    return self;
+  });
+});
+
+// > .ModuleName "GroupBy"
+Lapiz.Module("GroupBy", ["Collections"], function($L){
+  var _groupSettings = $L.Map();
+  _groupSettings.DoNotListen = true;
+
+  // > Lapiz.GroupBy(accessor, field)
+  // > Lapiz.GroupBy(accessor, groupKeyFn(key, accessor) )
+  // Creates a set of sub-groups from an accessor. While not limited to this, it
+  // is particularly useful in representing one-to-many relationships. The
+  // accessor containing all the instances of the child class can be grouped by
+  // their parent key.
+  //
+  // The same thing could be accomplished with Filters, one per parent. The
+  // advantage of this is that it is more efficient. Using filters, when a child
+  // is added, removed or changed, each Filter instance will be invoked to
+  // handle the event - O(N) where N is the number of Filters which is the
+  // number of parent keys. With this, the event is handled once and on the
+  // groups that need to be notified of the change are. This is O(1) regardless
+  // of the number of parent keys.
+  //
+  // Undefined group keys will not be used, they will just be skipped and not
+  // belong to any group.
+  //
+  // The second argument can be a group key function. This needs to return a
+  // string that represents the group key. This can be used to normalize values,
+  // group by multiple fields or replace undefined with a string value so that
+  // those values are grouped.
+  $L.set($L, "GroupBy", function(accessor, fieldOrFunc){
+    var _groups = $L.Map();
+    var _keymap = $L.Map();
+
+    var _groupByFn = fieldOrFunc;
+    if ($L.typeCheck.str(fieldOrFunc)){
+      _groupByFn = function(key, accessor){
+        return accessor(key)[fieldOrFunc];
+      };
+    }
+    $L.typeCheck.func(_groupByFn, "GroupBy must be invoked with function or attriubte name");
+
+    // > groupBy(key)
+    // Returns the group associated with key, if it exists.
+    var self = function(key){
+      key = key.toString();
+      var group = _groups[key];
+      if (group === undefined){
+        group = $L.Group(accessor, _groupSettings);
+        _groups[key] = group;
+      }
+      return group.Accessor;
+    };
+
+    // > groupBy._cls
+    // Return Lapiz.GroupBy
+    $L.set(self, "_cls", $L.GroupBy);
+
+    // > groupBy.has(key)
+    // Returns a bool indicating if the groupBy contains a group for the key
+    $L.set.meth(self, function has(key){
+      return _groups[key] !== undefined;
+    });
+
+    // > groupBy.keys
+    // Returns an array of the keys.
+    $L.set.getter(self, function keys(){
+      return Object.keys(_groups);
+    });
+
+    // > groupBy.length
+    // Read-only property that returns the length
+    $L.set.getter(self, function length(){
+      return Object.keys(_groups).length;
+    });
+
+    function inFn(key, accessor){
+      key = key.toString();
+      var groupKey = _groupByFn(key, accessor);
+      if (groupKey === undefined){
+        return;
+      }
+      var group = _groups[groupKey];
+      if (group === undefined){
+        group = $L.Group(accessor, _groupSettings);
+        _groups[groupKey] = group;
+      }
+      group.Add(key);
+      _keymap[key] = groupKey;
+    }
+
+    function remFn(key, accessor, oldVal){
+      key = key.toString();
+      var groupKey = _keymap[key];
+      if (groupKey === undefined){
+        return;
+      }
+      var group = _groups[groupKey];
+      group.Remove(key, oldVal);
+      delete _keymap[key];
+    }
+
+    function changeFn(key, accessor, oldVal){
+      key = key.toString();
+      var group;
+      
+      var oldGroupKey = _keymap[key];
+      var newGroupKey = _groupByFn(key, accessor);
+
+      if (oldGroupKey !== undefined){
+        group = _groups[oldGroupKey];
+        if (oldGroupKey === newGroupKey){
+          group.Change(key, oldVal);
+          return;
+        }
+        group.Remove(key, oldVal);
+      }
+      
+      if (newGroupKey === undefined){
+        return;
+      }
+      group = _groups[newGroupKey];
+      if (group === undefined){
+        group = $L.Group(accessor, _groupSettings);
+        _groups[newGroupKey] = group;
+      }
+      group.Add(key);
+      _keymap[key] = newGroupKey;
+    }
+
+    accessor.on.insert(inFn);
+    accessor.on.remove(remFn);
+    accessor.on.change(changeFn);
+
+    // > groupBy.ForceRescan()
+    // Rescans all values from parent access and fires insert and remove events
+    $L.set.meth(self, function ForceRescan(){
+      accessor.each(function(val, key){
+        var oldGroupKey, group;
+        key = key.toString();
+        var newGroupKey = _groupByFn(key, accessor);
+
+        if ($L.Map.has(_keymap, key)){
+          oldGroupKey = _keymap[key];
+          if (oldGroupKey === newGroupKey){
+            return;
+          }
+
+          group = _groups[oldGroupKey];
+          group.Remove(key);
+        }
+
+        if (newGroupKey === undefined){
+          return;
+        }
+        group = _groups[newGroupKey];
+        if (group === undefined){
+          group = $L.Group(accessor, _groupSettings);
+          _groups[newGroupKey] = group;
+        }
+        group.Add(key);
+        _keymap[key] = newGroupKey;
+      });
+    });
+
+    accessor.each(function(val, key){
+      key = key.toString();
+      var groupKey = _groupByFn(key, accessor);
+      if (groupKey === undefined){
+        return;
+      }
+      var group = _groups[groupKey];
+      if (group === undefined){
+        group = $L.Group(accessor, _groupSettings);
+        _groups[groupKey] = group;
+      }
+      group.Add(key);
+      _keymap[key] = groupKey;
+    });
+
     return self;
   });
 });
--- tests/grouper.js
+++ tests/grouper.js
@@ -18,14 +18,13 @@
     }
   };
 
-  var fruit = ["apple" ,"bananna" ,"cantaloup" ,"dates" ,"elderberry"];
-
   Lapiz.Test("Group/AddRemove", ["Dictionary/"], function(t){
     var dict = Lapiz.Dictionary(data);
     var group = Lapiz.Group(dict);
 
     !group.has(1)               || t.error("Did not expect 1");
     group.Add(1);
+    group.Add(1); // test double add
     group.has(1)                || t.error("Expected 1");
     group.keys.length === 1     || t.error("Expected length 1");
     group(1).name === "Stephen" || t.error("Expected 'Stephen'");
@@ -135,5 +134,164 @@
 
     !changed                      || t.error("Should not have fired change");
     group(4).name === "Alexander" || t.error("Should still reference accessor");
+  });
+
+  Lapiz.Test("Group/DoNotListen", ["Group/Change"], function(t){
+    var dict = Lapiz.Dictionary(data);
+    var group = Lapiz.Group(dict, {"DoNotListen": true});
+    group.Add(1);
+    group.Add(3);
+    group.Add(4);
+    var changed = false;
+    group.on.change(function(key){
+    	changed = true;
+    });
+    dict(4, {
+      name: "Alexander", 
+      fruit: "dragon fruit"
+    });
+
+    !changed                      || t.error("Should not have fired change");
+    group(4).name === "Alexander" || t.error("Should still reference accessor");
+    group.kill === undefined      || t.error("Should not have kill method");
+  });
+
+  Lapiz.Test("Group/Accessor", ["Group/AddRemove"], function(t){
+    var dict = Lapiz.Dictionary(data);
+    var group = Lapiz.Group(dict);
+    group.Add(1);
+    group.Add(2);
+    group.Add(4);
+
+    var acc = group.Accessor;
+    group.Add !== undefined || t.error("Should be defined");
+    acc.Add === undefined   || t.error("Should be undefined");
+    acc(4).name === "Alex"  || t.error("Expected 'Alex'");
+  });
+})();
+
+(function(){
+  var data = {
+    37: {
+      name: "Stephen",
+      role: "admin"
+    },
+    66: {
+      name: "Lauren",
+      role: "editor"
+    },
+    19: {
+      name: "Adam", 
+      role: "admin"
+    },
+    38:{
+      name: "anonymous"
+    },
+    68: {
+      name: "Alex", 
+      role: "user"
+    },
+    39: {
+      name: "Nathan",
+      role: "editor"
+    },
+    53: {
+      name: "unregistered",
+    },
+    76: {
+      name: "Chris",
+      role: "user"
+    }
+  };
+
+  Lapiz.Test("GroupBy/GroupBy", ["Group/"], function(t){
+    var dict = Lapiz.Dictionary(data);
+    var groupBy = Lapiz.GroupBy(dict, "role");
+    
+    var admins = groupBy("admin");
+    admins.length === 2 || t.error("Expected two admins");
+
+    groupBy.has("editor") || t.error("Expected 'editor' group");
+    !groupBy.has("hr")    || t.error("Did not expect 'hr' group");
+
+    groupBy.keys.indexOf("user") > -1 || t.error("Expected keys to contain user");
+    groupBy.keys.length === 3 || t.error("Expected three keys");
+    groupBy.length === 3      || t.error("Expected length of three");
+  });
+
+  Lapiz.Test("GroupBy/Events", ["GroupBy/GroupBy"], function(t){
+    var dict = Lapiz.Dictionary();
+    var groupBy = Lapiz.GroupBy(dict, "role");
+    
+    var admins = groupBy("admin");
+    var added, removed, changed;
+    admins.on.insert = function(key, accessor){
+      added = accessor(key);
+    };
+    admins.on.remove = function(key, accessor, oldVal){
+      removed = oldVal;
+    };
+    admins.on.change = function(key, accessor, oldVal){
+      changed = key;
+    };
+
+    // just testing that some paths don't blow up
+    dict(53, data[53]);
+    dict(91, {name: "anonymous"});
+    dict(53, {name: "anonymous"});
+    dict(91, {name: "anonymous", role: "editor"});
+
+    dict(68, data[68]);
+    groupBy("user").length === 1 || t.error("Expected one user");
+
+    dict(37, data[37]);
+    added === data[37]   || t.error("Expected added to be Stephen");
+
+    dict(66, data[66]);
+    added === data[37]   || t.error("Expected added to be still be Stephen");
+
+    dict(19, data[19]);
+    added === data[19]   || t.error("Expected added to be Adam");
+
+    dict.remove(53);
+
+    dict.remove(37);
+    removed === data[37] || t.error("Expected removed to be Stephen");
+
+    dict(19, {
+      name: "The Beast",
+      role: "admin"
+    });
+    changed === "19"     || t.error("Expected changed to be 19");
+  });
+
+  Lapiz.Test("GroupBy/Rescan", ["GroupBy/Events"], function(t){
+    var dict = Lapiz.Dictionary(data);
+    var groupBy = Lapiz.GroupBy(dict, "role");
+    
+    var admins = groupBy("admin");
+    var editors = groupBy("editor");
+    admins.length === 2  || t.error("Expected two admins");
+    editors.length === 2 || t.error("Expected two editors");
+
+    var added = 0;
+    var removed = 0;
+    admins.on.insert = function(key, accessor){
+      added++;
+    }
+    editors.on.remove = function(key, accessor, oldVal){
+      removed++;
+    }
+
+    data[66].role = "admin";
+    data[39].role = "avenger";
+    admins.length === 2 || t.error("Still expected two admins");
+    groupBy.ForceRescan();
+    admins.length === 3 || t.error("Expected three admins");
+    removed === 2       || t.error("Expected remove to fire twice, got "+removed);
+    added === 1         || t.error("Expected added to fire once, got "+added);
+    
+    // reset data
+    data[66].role = "editor";
   });
 })();
